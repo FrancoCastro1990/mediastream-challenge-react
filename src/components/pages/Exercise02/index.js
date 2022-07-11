9b0f8213e7af04d2798fc9ceb184e513896a92a1
Franco Castro V <franco.castro.villanueva.90@gmail.com>
2022-07-10 20:04:16 -0400

    add style in exercise 2

--- src/components/pages/Exercise02/index.js
+++ src/components/pages/Exercise02/index.js
@@ -14,11 +14,15 @@
 
 import "./assets/styles.css";
 import { useEffect, useState } from "react";
+import background from "./assets/mountains.jpeg";
 
-export default function Exercise02 () {
+export default function Exercise02() {
   const [movies, setMovies] = useState([])
+  const [filterMovies, setFilterMovies] = useState([])
+
   const [fetchCount, setFetchCount] = useState(0)
   const [loading, setLoading] = useState(false)
+  const [genres, setGenres] = useState([]);
 
   const handleMovieFetch = () => {
     setLoading(true)
@@ -28,6 +32,7 @@ export default function Exercise02 () {
       .then(res => res.json())
       .then(json => {
         setMovies(json)
+        setFilterMovies(json)
         setLoading(false)
       })
       .catch(() => {
@@ -35,42 +40,79 @@ export default function Exercise02 () {
       })
   }
 
+  const handleMovieFilterFetch = () => {
+    setLoading(true)
+    setFetchCount(fetchCount + 1)
+    console.log('Getting movies')
+    fetch('http://localhost:3001/genres')
+      .then(res => res.json())
+      .then(json => {
+        setGenres(json)
+        setLoading(false)
+      }).catch(() => {
+        console.log('Run yarn movie-api for fake api')
+      })
+  }
+
   useEffect(() => {
     handleMovieFetch()
-  }, [handleMovieFetch])
+    handleMovieFilterFetch()
+  }, [])
+
 
   return (
     <section className="movie-library">
-      <h1 className="movie-library__title">
-        Movie Library
-      </h1>
-      <div className="movie-library__actions">
-        <select name="genre" placeholder="Search by genre...">
-          <option value="genre1">Genre 1</option>
-        </select>
-        <button>Order Descending</button>
-      </div>
-      {loading ? (
-        <div className="movie-library__loading">
-          <p>Loading...</p>
-          <p>Fetched {fetchCount} times</p>
+      <img src={background} alt="background" className="movie-library__background" />
+      <div
+        className="movie-library__container"
+      >
+
+        <h1 className="movie-library__title">
+          Movie Library
+        </h1>
+        <div className="movie-library__actions">
+          <select name="genre" placeholder="Search by genre..."
+            onChange={(e) => {
+              const genre = e.target.value
+              if (genre === 'all') {
+                setFilterMovies(movies)
+              } else {
+                setFilterMovies(movies.filter(movie => movie.genres.includes(genre)))
+              }
+            }}
+          >
+            <option value="all">All</option>
+            {genres.map((genre, index) => <option key={`movie_library_list_${index}`} value={genre}>{genre}</option>)}
+          </select>
+          <button
+            onClick={() => {
+              const filterByYear = filterMovies.sort((a, b) => Number(a.year) - Number(b.year)).map(movie => movie);
+              setFilterMovies(filterByYear);
+            }}
+          >Order Descending</button>
         </div>
-      ) : (
-        <ul className="movie-library__list">
-          {movies.map(movie => (
-            <li key={movie.id} className="movie-library__card">
-              <img src={movie.posterUrl} alt={movie.title} />
-              <ul>
-                <li>ID: {movie.id}</li>
-                <li>Title: {movie.title}</li>
-                <li>Year: {movie.year}</li>
-                <li>Runtime: {movie.runtime}</li>
-                <li>Genres: {movie.genres.join(', ')}</li>
-              </ul>
-            </li>
-          ))}
-        </ul>
-      )}
+        {loading ? (
+          <div className="movie-library__loading">
+            <p>Loading...</p>
+            <p>Fetched {fetchCount} times</p>
+          </div>
+        ) : (
+          <div className="movie-library__list">
+            {filterMovies.map(movie => (
+              <div key={`movie_library_list_${movie.id}`} className="movie-library__card">
+                <img src={movie.posterUrl} alt={movie.title} />
+                <div className="movie-library__card-backbround">
+                </div>
+                <ul>
+                  <li>{movie.title}</li>
+                  <li>{movie.genres.join(', ')}</li>
+                  <li>{movie.year}</li>
+                </ul>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </section>
   )
 }
